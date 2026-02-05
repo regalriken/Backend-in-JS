6ac04b31b8fe43bb1d055a358939f9ada6b5f590
regalriken <rikenpatel2005@gmail.com>
2026-02-05 15:01:30 -0500

    added new Utils

--- Main Files/src/index.js
+++ Main Files/src/index.js
@@ -3,23 +3,12 @@ import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 
 dotenv.config({ path: "./env" });
-connectDB();
-
-// import express from "express";
-
-// const app = express();(async () => {
-//   try {
-//     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-//     app.on("error", (error) => {
-//       console.log("ERROR", error);
-//       throw error;
-//     });
-
-//     app.listen(process.env.PORT, () => {
-//       console.log("Process Listening on port", process.env.PORT);
-//     });
-//   } catch (error) {
-//     console.log("ERROR:", error);
-//     throw error;
-//   }
-// })();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log("Server is running at", process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log("DB connection failed", error);
+  });
